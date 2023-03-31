016ec41074aaccc9e5c0a6f7f47bfa8391a416f4
priange <ityakup@gmail.com>
2023-03-31 22:35:54 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    answer=a+b
+    return answer
+ 
+def subtract(a,b):
+    result=a-b
+    return result
+
+def multiply(a,b):
+    result2= a*b
+    return result2
+
+def divide(a,b):
+    lastresult=a/b
+    return lastresult
+
